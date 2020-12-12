98404e484d4750be8f53a1e903e561ed037eb49c
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2020-12-12 15:42:18 +0100

    15.4 Using props. Add three columns with titles.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import Column from '../Column/Column';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    image: PropTypes.string.isRequired,
+    children: PropTypes.node,
+    columns: PropTypes.array,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+  
   render() {
     return (
-      <section className={styles.component}>
-        <Hero />   
+    <section className={styles.component}>
+      <Hero titleText={this.props.title} imageAddress={this.props.image} />
+      <div className={styles.description}>
+        {this.props.children}
+      </div>
+      <div className={styles.columns}>
+        <Column columns={this.props.columns[0]} />
+        <Column columns={this.props.columns[1]} />
+        <Column columns={this.props.columns[2]} />
+      </div>
     </section>
     )
   }
